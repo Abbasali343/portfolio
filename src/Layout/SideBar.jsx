6b2178d38c39a4bc3f1357c7d939a1db97a9e2f1
Abbasali343 <60543462+Abbasali343@users.noreply.github.com>
2023-11-30 14:40:03 +0500

    made some changes

--- src/Layout/SideBar.jsx
+++ src/Layout/SideBar.jsx
@@ -1,9 +1,46 @@
-import { useState } from "react";
+import Resizer from "react-image-file-resizer";
+import { useState, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
 import profileImage from "../assets/images/profile-image.jpg";
 import "../assets/styles/Home.css";
 
-export default function SideBar({ isActive, handleScroll, scrollAmount,link }) {
+export default function SideBar({
+  isActive,
+  handleScroll,
+  scrollAmount,
+  link,
+}) {
+  const [newImage, setNewImage] = useState();
+
+  useEffect(() => {
+    const fetchAndResizeImage = async () => {
+      try {
+        // Fetch the image as a Blob
+        const response = await fetch(link);
+        const blob = await response.blob();
+
+        // Resize the image using react-image-file-resizer
+        Resizer.imageFileResizer(
+          blob,
+          320,
+          200,
+          "png",
+          100,
+          0,
+          (uri) => {
+            setNewImage(uri);
+          },
+          "base64",
+          320,
+          200
+        );
+      } catch (error) {
+        console.error("Error fetching or resizing image:", error);
+      }
+    };
+    fetchAndResizeImage();
+  }, []);
+
   function links(index, name, id, amount) {
     return (
       <div className="clickable-container">
@@ -14,7 +51,6 @@ export default function SideBar({ isActive, handleScroll, scrollAmount,link }) {
           }}
           id={isActive === index ? "active" : null}
           className="clickable"
-          // href={id}
         >
           {name}
         </a>
@@ -32,7 +68,11 @@ export default function SideBar({ isActive, handleScroll, scrollAmount,link }) {
   return (
     <>
       <div className="sidebar-container">
-        <img src={link} className="sidebar-image" />
+        <img
+          src={link}
+          id="profile-image-sidebar"
+          className="sidebar-image"
+        />
         <div>
           {links("main", "HOME", "#main")}
           {links("about-us", "about-us", "#about-us")}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,29 @@
-
+import { useState,useEffect } from "react";
+import Resizer from "react-image-file-resizer";
 import profileImage from "../assets/images/profile-image.jpg";
 import Introduction from "../Components/Introduction";
 import "../assets/styles/Home.css";
 
 export default function Home({ handleScroll,details }) {
+  const [newImage, setNewImage] = useState();
+
+  const profileImage = details.profile
   
+
+  useEffect(() => {
+    const fetchAndResizeImage = async () => {
+      try {
+        // Fetch the image as a Blob
+        const response = await fetch(details.profile);
+        const blob = await response.blob();
+
+      } catch (error) {
+        console.error("Error fetching or resizing image:", error);
+      }
+    };
+    fetchAndResizeImage();
+  }, []);
+  console.log(profileImage)
   return (
     <>
       <div className="container" id="main">
